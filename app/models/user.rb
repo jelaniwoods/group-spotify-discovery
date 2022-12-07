46ad445664e2565c96463f9ffb69b2c5dfa79a90
Jelani Woods <jelani@firstdraft.com>
2022-12-07 07:13:00 -0500

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # TODO refresh_credentials -> use refresh token to get new tokens
+  # do this when logging in
 end
