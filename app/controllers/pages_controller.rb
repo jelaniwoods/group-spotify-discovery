41481dd7049dfd6ffa4a230fb38d0792d7153e23
Jelani Woods <jelani@firstdraft.com>
2022-12-10 21:57:22 -0600

    Basic test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    if current_user
+      @recently_played = current_user.log_recently_played
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # TODO refresh_credentials -> use refresh token to get new tokens
   # do this when logging in
+  has_many :logs
+
+  def log_recently_played
+    user = RSpotify::User.new(spotify_info)
+    tracks = user.recently_played(limit: 50)
+    tracks.each do |track|
+      t = Track.find_or_create_by(
+        name: track.name,
+        album_name: track.album.name,
+        image: track.album.images[0]["url"],
+        url: track.external_urls["spotify"]
+      )
+      logs.find_or_create_by(
+        track: t,
+        played_at: DateTime.parse(track.played_at)
+      )
+    end
+    logs
+  end
 end
