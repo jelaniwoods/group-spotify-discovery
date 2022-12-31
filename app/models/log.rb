58a2ff5a1c6a69966f1defcaa3296cbad2c0d65e
Jelani Woods <jelani@firstdraft.com>
2022-12-31 17:25:00 -0600

    Add annotate gem

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: logs
+#
+#  id         :bigint           not null, primary key
+#  played_at  :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  track_id   :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_logs_on_track_id  (track_id)
+#  index_logs_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (track_id => tracks.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Log < ApplicationRecord
   belongs_to :track
   belongs_to :user
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: tracks
+#
+#  id         :bigint           not null, primary key
+#  album_name :string
+#  image      :string
+#  name       :string
+#  url        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Track < ApplicationRecord
 end
