c41bd55aa29363054d2d253e6bbb79af3b17cbb8
Jelani Woods <jelani@firstdraft.com>
2023-03-07 18:33:00 -0600

    Correctly precompile chartkick and bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import 'bootstrap'
-import "chartkick"
-import "Chart.bundle"
+import * as bootstrap from "bootstrap"
+
+import "chartkick/chart.js"
